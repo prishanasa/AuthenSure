5f793a31b086e87fb97152670ec3cf26f2a1353e
Prisha Nasa <166138214+prishanasa@users.noreply.github.com>
2026-03-31 22:36:04 +0530

    Correct kycRoutes import syntax
    Fix typo in kycRoutes import statement

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { userRateLimiter, adminRateLimiter } = require("../utils/rateLimiter");
 // Import individual route modules
 const userRoutes = require("./user"); // User routes
 const adminRoutes = require("./admin"); // Admin routes
-const kycRoutes = require("./kyc"); // Kyc routes
+const kycRoutes = :require("./kyc"); // Kyc routes
 
 // Use the routes
 router.use("/users", userRateLimiter, userRoutes);
